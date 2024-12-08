4b3adb0b0f3a3e34b3fec19a2594d09a3be5dc90
Josue Rojas <josuemonro@gmail.com>
2024-12-07 22:12:51 -0600

    first commit

---
+++ src/main/java/org/example/proyecto_intercambio/ProyectoIntercambioApplication.java
@@ -0,0 +1,13 @@
+package org.example.proyecto_intercambio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIntercambioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoIntercambioApplication.class, args);
+    }
+
+}
